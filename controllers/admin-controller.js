1c9c0924b3877d0424cc518ca45f010840cf236d
travel15740 <travel15740@gmail.com>
2024-05-20 14:40:50 +0800

    準備後台頁面、瀏覽後台列表的路由及controller

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,12 @@
 const adminController = {
   getFields: (req, res, next) => {
     return res.render('admin/fields.hbs')
+  },
+  getUsers: (req, res, next) => {
+    return res.render('admin/users.hbs')
+  },
+  getCategories: (req, res, next) => {
+    return res.render('admin/categories.hbs')
   }
 }
 
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -6,5 +6,9 @@ const adminController = require('../../controllers/admin-controller.js')
 
 // 設計路由
 router.get('/fields', adminController.getFields)
+router.get('/users', adminController.getUsers)
+router.get('/categories', adminController.getCategories)
+
+router.use('/', (req, res) => res.redirect('admin/fields'))
 
 module.exports = router
