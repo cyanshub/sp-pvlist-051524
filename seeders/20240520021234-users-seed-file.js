15dc76e7a5d9c4e98a974a81a26a4498b9cf043d
travel15740 <travel15740@gmail.com>
2024-05-20 10:31:38 +0800

    設計與加入使用者種子資料

---
+++ seeders/20240520021234-users-seed-file.js
@@ -0,0 +1,38 @@
+'use strict'
+const bcrypt = require('bcryptjs')
+const salt = 10
+
+// 載入環境變數
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('users', [{
+      email: 'root@example.com',
+      password: await bcrypt.hash(process.env.USER_PASSWORD, salt),
+      is_admin: true,
+      created_at: new Date(),
+      updated_at: new Date()
+    }, {
+      email: 'user1@example.com',
+      password: await bcrypt.hash(process.env.USER_PASSWORD, salt),
+      is_admin: false,
+      created_at: new Date(),
+      updated_at: new Date()
+    }, {
+      email: 'user2@example.com',
+      password: await bcrypt.hash(process.env.USER_PASSWORD, salt),
+      is_admin: false,
+      created_at: new Date(),
+      updated_at: new Date()
+    }
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('users', null, {})
+  }
+}
