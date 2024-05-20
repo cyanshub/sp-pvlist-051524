879165aba6d3c3f90881603c4a3a8214a7844904
travel15740 <travel15740@gmail.com>
2024-05-20 16:08:38 +0800

    新增分類種子資料

---
+++ seeders/20240520075141-categories-seed-file.js
@@ -0,0 +1,20 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('categories',
+      ['地面型系統', '廠房鐵皮屋頂', '建築水泥樓面屋頂', '畜禽舍屋頂', '校園屋頂', '其他屋頂']
+        .map(item => {
+          return {
+            name: item,
+            created_at: new Date(),
+            updated_at: new Date()
+          }
+        })
+    )
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('categories', null, {})
+  }
+}
