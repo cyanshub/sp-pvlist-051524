4a1994bc693bc968a0e69bafe90271de93418730
travel15740 <travel15740@gmail.com>
2024-05-30 15:33:47 +0800

    建立網站前台頁面標籤與設計路由

--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,47 @@ const admin = require('./modules/admin.js')
 // 載入 controller 資料夾
 const fieldController = require('../controllers/field-controller.js')
 const userController = require('../controllers/user-controller.js')
+const commentController = require('../controllers/comment-controller.js')
 
 // 載入 middleware
-const { generalErrorHandler } = require('../middleware/error-handler.js')
-const passport = require('../config/passport.js')
-const { authenticated, authenticatedAdmin } = require('../middleware/auth.js')
-
-router.use('/admin', authenticatedAdmin, admin)
+const { generalErrorHandler } = require('../middleware/error-handler.js') // 報錯
+const passport = require('../config/passport.js') // 使用者登入及識別
+const { authenticated, authenticatedAdmin } = require('../middleware/auth.js') // 負責驗證
+const upload = require('../middleware/multer.js') // 負責上傳圖片
 
 // 設計路由
+// 設計路由: 後台區域
+router.use('/admin', authenticatedAdmin, admin)
+
+// 設計路由: 使用者相關
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 直接使用passport提供的方法進行登入驗證
 router.get('/logout', userController.logOut)
+// router.get('/users/top', authenticated, userController.getTopUsers)
+// router.post('/favorite/:fieldId', authenticated, userController.addFavorite)
+// router.delete('/favorite/:fieldId', authenticated, userController.removeFavorite)
+// router.post('/following/:userId', authenticated, userController.addFollowing)
+// router.delete('/following/:userId', authenticated, userController.removeFollowing)
+// router.get('/users/:id', authenticated, userController.getUser)
+// router.get('/users/:id/edit', authenticated, userController.editUser)
+// router.put('/users/:id', authenticated, upload.single('avatar'), userController.putUser)
 
+// 設計路由: 案場相關
 router.get('/fields', authenticated, fieldController.getFields)
+// router.get('/fields/feeds', authenticated, fieldController.getFeeds)
+// router.get('/fields/favorites', authenticated, fieldController.getFavorites)
+// router.get('/fields/top', authenticated, fieldController.getTopFields)
+// router.get('/fields/trec', authenticated, fieldController.getTrecs)
+// router.get('/fields/:id', authenticated, fieldController.getField)
+// router.get('/fields/:id/dashboard', authenticated, fieldController.getDashboard)
+
+// 設計路由: 留言功能相關
+// router.post('/comments', authenticated, commentController.postComment)
+// router.delete('/comments/:id', authenticated, commentController.deleteComment)
+
+// 設計路由: 錯誤相關
 router.get('/', (req, res) => res.redirect('/fields'))
 router.use('/', generalErrorHandler)
 
