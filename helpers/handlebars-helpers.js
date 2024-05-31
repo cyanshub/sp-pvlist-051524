792f0416aacab5d0c2a8e8623d256f18b42e3812
travel15740 <travel15740@gmail.com>
2024-05-31 16:59:44 +0800

    顯示人性化時間

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,3 +1,8 @@
+// 載入所需的 npm 套件
+const dayjs = require('dayjs')
+const relativeTime = require('dayjs/plugin/relativeTime')
+dayjs.extend(relativeTime)
+
 module.exports = {
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
@@ -11,5 +16,7 @@ module.exports = {
 
     // 格式化數字為千分位表示
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-  }
+  },
+
+  relativeTimeFromNow: a => dayjs(a).fromNow()
 }
