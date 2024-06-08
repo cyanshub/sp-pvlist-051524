bb7b6a8430499fa09b6dafc5679ec85e84f94f32
travel15740 <travel15740@gmail.com>
2024-06-09 00:17:59 +0800

    將專案佈署到heroku遠端應用程式伺服器

--- migrations/20240531063934-create-comment.js
+++ migrations/20240531063934-create-comment.js
@@ -1,8 +1,8 @@
-'use strict';
+'use strict'
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Comments', {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('comments', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -26,9 +26,9 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    })
   },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Comments');
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('comments')
   }
-};
+}
